373d6abb03cd7be1a358558b41b8f1f03a86d183
Naveen Palaniswamy <8531695+palanisn@users.noreply.github.com>
2018-02-24 07:28:17 +1100

    Update migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Owned = artifacts.require("./Owned.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Owned);
 };
