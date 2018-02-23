277a3d0fde08933bae7792553981698fd11a10b6
Naveen Palaniswamy <8531695+palanisn@users.noreply.github.com>
2018-02-23 11:16:31 +1100

    Adding Owned contract

---
+++ contracts/Owned.sol
@@ -0,0 +1,37 @@
+/**
+@title Owned contract - Assigns ownership of contract to creator
+@author Naveen Palaniswamy
+@copyright Naveen Palaniswamy
+*/
+
+pragma solidity 0.4.20;
+
+
+contract Owned {
+
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /// Constructor function for Owned contract
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+    @notice Function that facilitates transfer of ownership from current owner to a new owner.
+            Requires the current owner to be the function executor.
+    @param _newOwner New Owner address
+    */
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
+
+}
